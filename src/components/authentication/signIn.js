05e5f980ca7596fe3c76387885e9633d01f140ef
Barış ÖZDEMİRCİ <ozdemircibaris@gmail.com>
2020-08-13 09:06:27 +0300

    building the architecture

---
+++ src/components/authentication/signIn.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export default class SignIn extends Component {
+    render() {
+        return (
+            <View>
+                <Text> SignIn </Text>
+            </View>
+        )
+    }
+}
