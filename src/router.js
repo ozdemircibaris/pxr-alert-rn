4f1c2daaa57dab0b72c8999c4c1b802d53ffd98d
Barış ÖZDEMİRCİ <ozdemircibaris@gmail.com>
2020-08-13 09:10:51 +0300

    routing setups

---
+++ src/router.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Router, Stack, Scene } from 'react-native-router-flux';
+import SignIn from './components/authentication/signIn';
+import SignUp from './components/authentication/signUp';
+import Index from './components/pages';
+
+export default class RouterComp extends Component {
+    render() {
+        return (
+            <Router>
+                <Stack key="root">
+                    <Stack key="auth">
+                        <Scene
+                            key="signIn"
+                            component={SignIn} />
+                        <Scene
+                            key="signUp"
+                            component={SignUp} />
+                    </Stack>
+                    <Stack key="main">
+                        <Scene
+                            key="index"
+                            component={Index} />
+                    </Stack>
+                </Stack>
+            </Router>
+        )
+    }
+}
