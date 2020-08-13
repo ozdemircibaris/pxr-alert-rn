e4a3cb1479c4c265db842b18c76985e6d487d65f
selinsnmz <selinsonmez111@gmail.com>
2020-08-13 16:58:28 +0300

    Test

--- src/components/authentication/signUp.js
+++ src/components/authentication/signUp.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, Button } from 'react-native'
 
 export default class SignUp extends Component {
     render() {
