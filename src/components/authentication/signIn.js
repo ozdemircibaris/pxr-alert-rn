706710d7275ca19f9aa03990dd592227bafafefb
perseusss <ed>
2020-08-26 18:34:31 +0300

    signin page process

--- src/components/authentication/signIn.js
+++ src/components/authentication/signIn.js
@@ -2,47 +2,45 @@ import React, { Component } from 'react'
 import { Text, View,StyleSheet, TouchableOpacity,Button,SafeAreaView ,TextInput, Image} from 'react-native'
 import { Actions } from 'react-native-router-flux'
 import LinearGradient from 'react-native-linear-gradient';
+import {PhoneWidth , PhoneHeight} from '../config/env';
+
 
 export default class SignIn extends Component {
     render() {
         return (
             
 <View style = {styles.container}>
-    <LinearGradient colors={['#4d21d0', '#621fb9', '#8825b9']} style={styles.linearGradient}>
-    <Image style={styles.ImageWork} source={require('../authentication/work.png')} />
-        <View style={styles.whiteBox}>
-          <Text style={{fontSize:25}}>Girmek için parolayı söyle</Text>
-       
-               
-          <View style={styles.inputsBox}>
-          <TextInput
-                style={{textAlign:'center' , width: '90%',height: 40, borderColor: '#00D1DB', borderWidth: 2 ,margin:40}}
-                onChangeText={text => onChangeText(text)}
-                placeholder='emailinizi giriniz'
-                placeholderTextColor='black'
-                
-              />
-          <TextInput
-              style={{  textAlign:'center',color:'black', width: '90%' ,height: 40, borderColor: '#00D1DB', borderWidth: 2 }}
-              onChangeText={text => onChangeText(text)}
-              placeholder='şifrenizi giriniz'
-              placeholderTextColor='black'
-              
-            />
-   
-        </View> 
-       
-        </View> 
-              <View style={styles.buttonBox}> 
-              <Button style={styles.LoginBtn}
-                onPress={Actions.Main()}
-                title="giriş yap"
-                color="white"
-                accessibilityLabel="Learn more about this purple button"
-              />  
-            </View>
+ <LinearGradient colors={['#4d21d0', '#621fb9', '#8825b9']} style={styles.linearGradient}>
+    <Image style ={styles.image} source={require('../pages/color-circle.png')}/>
+  <View style={styles.whiteBox}>
+    <Text style={styles.txt}>Girmek için parolayı söyle</Text>
+
+    <TextInput
+      style={styles.userNameWrapper}
+      onChangeText={text => onChangeText(text)}
+      placeholder='emailinizi giriniz'
+      placeholderTextColor='black'
+        />
+
+    <TextInput
+      style={styles.passwordWrapper}
+      onChangeText={text => onChangeText(text)}
+      placeholder='şifrenizi giriniz'
+      placeholderTextColor='black'
+        />
+
+      <View style ={styles.buttonBox}>
+        <TouchableOpacity style={styles.LoginBtn}
+          onPress={Actions.Main()}>
+          <Text style={styles.loginTxt}>Giriş Yap</Text>
+        </TouchableOpacity>
+
+      </View>
+    <Text style={styles.noAccountTxt}>Hesabın yok di mi ? Üye Ol !</Text>
+          
+  </View> 
         
-    </LinearGradient>
+ </LinearGradient>
    
 </View>
         )
@@ -52,44 +50,67 @@ const styles = StyleSheet.create({
     container: {
       flex: 1,
       flexDirection: "column",
-   
-
-    },linearGradient: {
-      height:'100%',
+    }
+    ,linearGradient: {
+      height:PhoneHeight*1,
       paddingLeft: 15,
       paddingRight: 15,
       borderRadius: 2,
-    },LoginBtn:{
-        zIndex: 3 ,
-        position: 'absolute',
-        height: 40, 
-        width: '100%' ,
-    },
-    buttonBox:{
-       position: 'absolute',
-       zIndex: 3 ,
-       marginTop:675,
-       backgroundColor:'#00D1DB',
-       marginLeft:45,
-       width: '85%' ,
-    },
-    inputsBox:{
-      alignItems:'center'
-    },
-    whiteBox:{
-      backgroundColor:'white',
-      paddingLeft: 13,
-      paddingRight: 13,
-      height:300,
-      borderRadius:30,
-      marginTop:310,
-      
-    },ImageWork:{
-      height:70,
-      width:60,
-      marginTop:50,
-      marginLeft:160
+      justifyContent:'center',
     }
-    
+    ,txt:{
+      fontSize:23,
+    },image:{
+      height: PhoneHeight * 0.23,
+      width: PhoneWidth * 0.5,
+      marginLeft:'24%',
+      marginBottom:20
+    }
+    ,LoginBtn:{
+      backgroundColor:'#94d569',
+      height: PhoneHeight * 0.04,
+      width: PhoneWidth * 0.5,
+      borderRadius: 10,
+      marginTop:'5%'
+    }
+    ,inputsBox:{
+      alignItems:'center',
+      justifyContent:'center',
+      padding:'15%',
+    }
+    ,whiteBox:{
+      alignSelf:'center',
+      alignItems:'center',
+      backgroundColor:'white',
+      borderRadius:16,
+      height: PhoneHeight * 0.4,
+      width: PhoneWidth * 0.95,
+      marginTop:'20%', 
+    }
+    ,userNameWrapper:{ 
+      marginTop:'23%',
+      textAlign:'center',
+      borderWidth:1.5,
+      height: PhoneHeight * 0.05,
+      width: PhoneWidth * 0.7,
+      borderColor:'#94d569',
+      borderRadius: 10,
+    }
+    ,passwordWrapper:{
+      marginTop:'5%',
+      textAlign:'center',
+      borderWidth:1.5,
+      height: PhoneHeight * 0.05,
+      width: PhoneWidth * 0.7,
+      borderColor:'#94d569',
+      borderRadius: 10,
+    }
+    ,loginTxt:{
+      alignSelf:'center',
+      marginTop:9,
+    }
+    ,noAccountTxt:{
+      //to last text of the page
+    }
 
   });
