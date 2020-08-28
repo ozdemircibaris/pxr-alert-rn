819d83db135518be6b169f9a423eeb5dfc285cb2
perseusss <ed>
2020-08-28 13:31:00 +0300

    tried to use flatlist

--- src/components/pages/Page3.js
+++ src/components/pages/Page3.js
@@ -1,34 +1,62 @@
 import React, { Component } from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View, Text, SafeAreaView,FlatList,StatusBar} from 'react-native';
 import Header from '../leftBar/Header';
 
 
-export default class Page3 extends Component {
-    constructor(props) {
-        super(props);
-    }
-  render() {
-    return (
-        <View>
-            <Header />
-            <View style = {styles.container}>
-                <Text style = {styles.title}> Bana Gelen Görevler </Text>
-            </View>
+  export default class Page3 extends Component {
+   DATA = [
+        {
+          id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+          title: 'First Item',
+        },
+        {
+          id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+          title: 'Second Item',
+        },
+        {
+          id: '58694a0f-3da1-471f-bd96-145571e29d72',
+          title: 'Third Item',
+        },
+      ];
+      
+      Item = ({ title }) => (
+        <View style={styles.item}>
+          <Text style={styles.title}>{title}</Text>
         </View>
-              
       );
-  }
-}
-
-let styles = StyleSheet.create({
-    container: {
-        padding: 20,
-        alignItems: 'center',
-        borderBottomWidth:1,
-        borderBottomColor: 'white',
-        flexDirection: 'row',
-     },
-     title:{
-      fontSize:17
-     },     
-});
+      
+       App = () => {
+        const renderItem = ({ item }) => (
+          <Item title={item.title} />
+        );
+      
+        return (
+          <SafeAreaView style={styles.container}>
+            <FlatList
+              data={DATA}
+              renderItem={renderItem}
+              keyExtractor={item => item.id}
+            />
+          </SafeAreaView>
+        );
+      }
+    
+     styles = StyleSheet.create({
+        container: {
+          flex: 1,
+          marginTop: StatusBar.currentHeight || 0,
+        },
+        item: {
+          backgroundColor: '#f9c2ff',
+          padding: 20,
+          marginVertical: 8,
+          marginHorizontal: 16,
+        },
+        title: {
+          fontSize: 32,
+        },
+      });
+    
+   
+    }
+    
